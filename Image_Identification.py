6ffed42b215fcb3bdb536b8a97f36a5670d0b1f5
haoran9 <30956090+haoran9@users.noreply.github.com>
2020-06-17 23:01:59 -0700

    Update Image_Identification.py

--- Image_Identification.py
+++ Image_Identification.py
@@ -1,72 +1,125 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score,mean_squared_error
-
-import sklearn.metrics as metrics
-import seaborn as sns
-
-# Import tools needed for visualization
-from sklearn.tree import export_graphviz
-from sklearn.tree import DecisionTreeClassifier
-import pydot
-import pydotplus
-from IPython.display import Image 
-
-def item_user(file, normalize= True)
-  df_x= df_dum.drop('FFH_88', axis = 1)
-  df_FFH_y = np.array(df_dum['FFH_88'])
-
-  # Saving feature names for later use
-  feature_list = list(df_x.columns)
-
-  X = data.iloc[:,3:14]  #independent columns
-  y = data.iloc[:,-1]    #target column i.e price range
-
-  #get correlations of each features in dataset
-  corrmat = df.corr()
-  top_corr_features = corrmat.index
-  plt.figure(figsize=(20,20))
-  #plot heat map
-  g=sns.heatmap(df[top_corr_features].corr(),annot=True,cmap="RdYlGn",linewidths=0.1,annot_kws={"size": 20})
-  
-  return corrmat
+import torch
+from torch import nn
+from torch import optim
+from torchvision import datasets, transforms, models
+from torch.utils.data.sampler import SubsetRandomSampler
 
 
+def load_split_train_test(datadir, valid_size = .2, batch_size=64):
+    train_transforms = transforms.Compose(
+        [
+            transforms.Resize(224),
+            transforms.ToTensor(),
+        ]
+    )
+    test_transforms = transforms.Compose(
+        [
+            transforms.Resize(224),
+            transforms.ToTensor(),
+        ]
+    )
+    train_data = datasets.ImageFolder(
+        datadir,
+        transform=train_transforms
+    )
+    test_data = datasets.ImageFolder(
+        datadir,
+        transform=test_transforms
+    )
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+    np.random.shuffle(indices)
+    train_idx, test_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    trainloader = torch.utils.data.DataLoader(
+        train_data,
+        sampler=train_sampler,
+        batch_size=batch_size
+    )
+    testloader = torch.utils.data.DataLoader(
+        test_data,
+        sampler=test_sampler,
+        batch_size=batch_size
+    )
+    return trainloader, testloader
 
-def data_split(path, save=True)
 
-  # Split the data into training and testing sets
-  train_data, test_data, train_FFH_88, test_FFH_88 = train_test_split(df_x, df_FFH_y, test_size = 0.2, random_state = 42)
+def train(data_dir, model_name='model', vis=False):
 
-  #test_FFE_88.describe()
-  train_data.describe()
-  
-  returen train_FFH_88
-  
-  
-# Instantiate model with 1000 decision trees
-rf = RandomForestRegressor(n_estimators = 100, random_state = 42)
+    trainloader, testloader = load_split_train_test(data_dir, .2)
+    print(trainloader.dataset.classes)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = models.resnet50(pretrained=True)
+    print(model)
 
-# Train the model on training data
-rf.fit(train_data, train_FFH_88);
+    for param in model.parameters():
+        param.requires_grad = False
 
-# Use the forest's predict method on the test data
-predictions = rf.predict(test_data)
+    model.fc = nn.Sequential(nn.Linear(2048, 512),
+                             nn.ReLU(),
+                             nn.Dropout(0.2),
+                             nn.Linear(512, 10),
+                             nn.LogSoftmax(dim=1))
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+    model.to(device)
 
-# Calculate the MSE
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(test_FFH_88, predictions)))
+    epochs = 100
+    steps = 0
+    running_loss = 0
+    print_every = 10
+    train_losses, test_losses = [], []
+    for epoch in range(epochs):
+        for inputs, labels in trainloader:
+            steps += 1
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in testloader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+                        test_loss += batch_loss.item()
+
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals =top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                train_losses.append(running_loss / len(trainloader))
+                test_losses.append(test_loss / len(testloader))
+                print("Epoch {} | Train loss {}.. ".format(epoch, running_loss))
+                print("         | Test  loss {}.. ".format(test_loss))
+                print("         | Accuracy   {}.. ".format(accuracy))
+
+                running_loss = 0
+                model.train()
+    torch.save(model, '{}.pth'.format(model_name))
+
+    if vis:
+        plt.plot(train_losses, label='Training loss')
+        plt.plot(test_losses, label='Validation loss')
+        plt.legend(frameon=False)
+        plt.show()
 
 
-# Get numerical feature importances
-importances = list(rf.feature_importances_)
-# List of tuples with variable and importance
-feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
-# Sort the feature importances by most important first
-feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
-# Print out the feature and importances 
-[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances];
+if __name__ == "__main__":
+    import sys
+    train(data_dir=sys.argv[1])
+
 
 
 
